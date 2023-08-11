2ca6b0a23c974af8b9a35364628305f10ceaa8f5
Özgür Çoban <cobanozgur55@gmail.com>
2023-08-11 12:48:55 +0300

    asd

--- server.js
+++ server.js
@@ -6,14 +6,7 @@ const cors = require("cors");
 const mongoose=require("mongoose")
 const express=require("express")
 const app=express()
-app.use(cors());
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://todolist-backend-odq2.onrender.com');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-    });
+
 
 const https = require('https').Server(app);
 
@@ -55,5 +48,12 @@ io.on('connection',(socket)=>{
 })
 
 
-
+app.use(cors());
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://todolist-backend-odq2.onrender.com');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+    });
 
